6e7b030315990afe12b0ac4b4a4d9e9914b448b6
Martynas <135633452+MartynasOnTop@users.noreply.github.com>
2023-11-05 17:40:21 +0200

    movement, bullet

---
+++ Assets/Script/Tank.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Tank : MonoBehaviour
+{
+    public float speed = 5;
+    public float rotateSpeed = 90;
+
+    public string vertical;
+    public string horizontal;
+
+    public KeyCode shootKey;
+
+    public GameObject bullet;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var ver = Input.GetAxis(vertical);
+        transform.position += transform.forward * speed * ver * Time.deltaTime;
+
+        var hor = Input.GetAxis(horizontal);
+        transform.Rotate(0, rotateSpeed * hor * Time.deltaTime, 0);
+
+        if (Input.GetKeyDown(shootKey))
+        {
+            Instantiate(bullet, transform.position, transform.rotation);
+        }
+    }
+}
