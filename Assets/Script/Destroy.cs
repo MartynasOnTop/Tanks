5db2cc1468ceccb7b6708f55414b4bb93f8376b3
Martynas <135633452+MartynasOnTop@users.noreply.github.com>
2023-11-08 22:42:02 +0200

    objects to pieces

--- Assets/Script/Destroy.cs
+++ Assets/Script/Destroy.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class Destroy : MonoBehaviour
 {
     public GameObject piece;
+    public GameObject leavePiece;
+    public GameObject woodPiece;
+
+    public Transform SpawnPoint1;
+    public Transform SpawnPoint2;
+    public Transform SpawnPoint3;
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.name.Contains("bullet"))
-        {
-            Instantiate(piece, transform.position, transform.rotation);
-        }
+        Destroy(gameObject);
+        Instantiate(leavePiece, SpawnPoint1.position, SpawnPoint1.rotation);
+        Instantiate(leavePiece, SpawnPoint2.position, SpawnPoint2.rotation);
+        Instantiate(woodPiece, SpawnPoint3.position, SpawnPoint3.rotation);
     }
 }
